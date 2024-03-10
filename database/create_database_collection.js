96cb0cf9dc571e7ee11dd153398c1b94ae50d4b1
emberow <chin45630@gmail.com>
2024-03-10 16:31:04 +0800

    add npm run start & initDB script

--- database/create_database_collection.js
+++ database/create_database_collection.js
@@ -1,25 +1,30 @@
-var MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 
-var url = "mongodb://localhost:27017/";
+const url = "mongodb://localhost:27017/";
 
-// 創建collection
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("chess_web");
-    dbo.createCollection("account", function(err, res) {
-      if (err) throw err;
-      console.log("Collection created!");
-      db.close();
-    });
-    dbo.createCollection("player", function(err, res) {
-      if (err) throw err;
-      console.log("Collection created!");
-      db.close();
-    });
-    dbo.createCollection("gaming_room", function(err, res) {
-      if (err) throw err;
-      console.log("Collection created!");
-      db.close();
-    });
-  });
+async function createCollections() {
+    try {
+        const client = await MongoClient.connect(url, { useNewUrlParser: true });
+        const dbo = client.db("chess_web");
 
+        // Create "account" collection
+        await dbo.createCollection("account");
+        console.log("Account collection created!");
+
+        // Create "player" collection
+        await dbo.createCollection("player");
+        console.log("Player collection created!");
+
+        // Create "gaming_room" collection
+        await dbo.createCollection("gaming_room");
+        console.log("Gaming room collection created!");
+
+        // Close the connection
+        await client.close();
+        console.log("Connection closed.");
+    } catch (err) {
+        console.error("Error:", err);
+    }
+}
+
+createCollections();
